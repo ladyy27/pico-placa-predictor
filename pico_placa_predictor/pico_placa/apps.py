3b6bf48b32e3124c67b9719201d7c33d99ad4339
Lady Yaguachi <ela@MacBook-Pro-de-Lady.local>
2020-03-03 16:40:10 -0500

    pico-placa-predictor server project added

---
+++ pico_placa_predictor/pico_placa/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PicoPlacaConfig(AppConfig):
+    name = 'pico_placa'
---
+++ pico_placa_predictor/pico_placa/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Create your views here.
+class PicoPlacaPredictorView(APIView):
+    def predict_pico_placa(self, licenseplate, datetime):
+        return
+    def get(self, request):
+        return
